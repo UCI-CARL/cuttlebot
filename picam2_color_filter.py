951c2c0ac9de796809986d2f527859c14525963c
mapfeiff <mapfeiff@uci.edu>
2023-07-28 19:15:30 -0700

    Added missing statement for getting out of infinite loop via key input and destroying all open windows

--- picam2_color_filter.py
+++ picam2_color_filter.py
@@ -118,5 +118,8 @@ for i in range(500):
     #plt.draw()
     #plt.pause(0.001)
     cv2.imshow("Red Filter Image Result", masked_image)
-    
+    if(cv2.getKey(1) == 27):
+        break
+
+cv2.destroyAllWindows()
 picam.stop()
