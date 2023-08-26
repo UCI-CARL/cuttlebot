c4b29b7c7605573a961be11269237d3e4eba256a
mapfeiff <mapfeiff@uci.edu>
2023-08-25 17:29:17 -0700

    changed color filter value

--- v2_PID_ColorTracking/main.py
+++ v2_PID_ColorTracking/main.py
@@ -6,7 +6,7 @@ def main():
     #instantiate vision object
     vision = Perception()
     #use vision object to track color
-    vision.track_color(0, precision=15)
+    vision.track_color(120, precision=15)
 
 #Condition to call main only when file is directly excuted
 if(__name__ == "__main__"):
