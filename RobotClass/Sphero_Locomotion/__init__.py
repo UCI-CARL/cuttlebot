98246731f55e58e6a1c8c370c18679204fb0b0ed
mapfeiff <mapfeiff@uci.edu>
2023-09-12 19:05:44 -0700

    changed init file to account for folder name change

--- RobotClass/Sphero_Locomotion/__init__.py
+++ RobotClass/Sphero_Locomotion/__init__.py
@@ -2,57 +2,57 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-from sphero_sdk.common.enums.colors_enums import Colors
-from sphero_sdk.common.enums.infrared_codes_enums import InfraredCodes
-from sphero_sdk.common.enums.rvr_led_groups_enum import RvrLedGroups
-from sphero_sdk.common.enums.sphero_rvr_enums import SpheroRvrTargets
-from sphero_sdk.common.enums.sphero_rvr_enums import SpheroRvrLedBitmasks
-from sphero_sdk.common.rvr_streaming_services import RvrStreamingServices
+from Sphero_Locomotion.common.enums.colors_enums import Colors
+from Sphero_Locomotion.common.enums.infrared_codes_enums import InfraredCodes
+from Sphero_Locomotion.common.enums.rvr_led_groups_enum import RvrLedGroups
+from Sphero_Locomotion.common.enums.sphero_rvr_enums import SpheroRvrTargets
+from Sphero_Locomotion.common.enums.sphero_rvr_enums import SpheroRvrLedBitmasks
+from Sphero_Locomotion.common.rvr_streaming_services import RvrStreamingServices
 
-from sphero_sdk.common.enums.api_and_shell_enums import ApiResponseCodesEnum
-from sphero_sdk.common.enums.drive_enums import RawMotorModesEnum
-from sphero_sdk.common.enums.drive_enums import MotorIndexesEnum as DriveMotorIndexesEnum
-from sphero_sdk.common.enums.drive_enums import DriveFlagsBitmask
-from sphero_sdk.common.enums.drive_enums import XyPositionDriveFlagsBitmask
-from sphero_sdk.common.enums.drive_enums import ControlSystemTypesEnum
-from sphero_sdk.common.enums.drive_enums import ControlSystemIdsEnum
-from sphero_sdk.common.enums.drive_enums import LinearVelocitySlewMethodsEnum
-from sphero_sdk.common.enums.io_enums import SpecdrumsColorPaletteIndiciesEnum
-from sphero_sdk.common.enums.power_enums import BatteryVoltageStatesEnum
-from sphero_sdk.common.enums.power_enums import BatteryVoltageReadingTypesEnum
-from sphero_sdk.common.enums.power_enums import AmplifierIdsEnum
-from sphero_sdk.common.enums.sensor_enums import MotorIndexesEnum as SensorMotorIndexesEnum
-from sphero_sdk.common.enums.sensor_enums import ThermalProtectionStatusEnum
-from sphero_sdk.common.enums.sensor_enums import TemperatureSensorsEnum
-from sphero_sdk.common.enums.sensor_enums import StreamingDataSizesEnum
-from sphero_sdk.common.enums.sensor_enums import GyroMaxFlagsBitmask
-from sphero_sdk.common.enums.sensor_enums import LocatorFlagsBitmask
-from sphero_sdk.common.enums.sensor_enums import InfraredSensorLocationsBitmask
+from Sphero_Locomotion.common.enums.api_and_shell_enums import ApiResponseCodesEnum
+from Sphero_Locomotion.common.enums.drive_enums import RawMotorModesEnum
+from Sphero_Locomotion.common.enums.drive_enums import MotorIndexesEnum as DriveMotorIndexesEnum
+from Sphero_Locomotion.common.enums.drive_enums import DriveFlagsBitmask
+from Sphero_Locomotion.common.enums.drive_enums import XyPositionDriveFlagsBitmask
+from Sphero_Locomotion.common.enums.drive_enums import ControlSystemTypesEnum
+from Sphero_Locomotion.common.enums.drive_enums import ControlSystemIdsEnum
+from Sphero_Locomotion.common.enums.drive_enums import LinearVelocitySlewMethodsEnum
+from Sphero_Locomotion.common.enums.io_enums import SpecdrumsColorPaletteIndiciesEnum
+from Sphero_Locomotion.common.enums.power_enums import BatteryVoltageStatesEnum
+from Sphero_Locomotion.common.enums.power_enums import BatteryVoltageReadingTypesEnum
+from Sphero_Locomotion.common.enums.power_enums import AmplifierIdsEnum
+from Sphero_Locomotion.common.enums.sensor_enums import MotorIndexesEnum as SensorMotorIndexesEnum
+from Sphero_Locomotion.common.enums.sensor_enums import ThermalProtectionStatusEnum
+from Sphero_Locomotion.common.enums.sensor_enums import TemperatureSensorsEnum
+from Sphero_Locomotion.common.enums.sensor_enums import StreamingDataSizesEnum
+from Sphero_Locomotion.common.enums.sensor_enums import GyroMaxFlagsBitmask
+from Sphero_Locomotion.common.enums.sensor_enums import LocatorFlagsBitmask
+from Sphero_Locomotion.common.enums.sensor_enums import InfraredSensorLocationsBitmask
 
-from sphero_sdk.asyncio.client.firmware.rvr_fw_check_async import RvrFwCheckAsync
-from sphero_sdk.asyncio.controls.led_control_async import LedControlAsync
-from sphero_sdk.asyncio.controls.drive_control_async import DriveControlAsync
-from sphero_sdk.asyncio.controls.infrared_control_async import InfraredControlAsync
-from sphero_sdk.asyncio.controls.sensor_control_async import SensorControlAsync
-from sphero_sdk.asyncio.client.toys.sphero_rvr_async import SpheroRvrAsync
-from sphero_sdk.asyncio.client.dal.serial_async_dal import SerialAsyncDal
+from Sphero_Locomotion.asyncio.client.firmware.rvr_fw_check_async import RvrFwCheckAsync
+from Sphero_Locomotion.asyncio.controls.led_control_async import LedControlAsync
+from Sphero_Locomotion.asyncio.controls.drive_control_async import DriveControlAsync
+from Sphero_Locomotion.asyncio.controls.infrared_control_async import InfraredControlAsync
+from Sphero_Locomotion.asyncio.controls.sensor_control_async import SensorControlAsync
+from Sphero_Locomotion.asyncio.client.toys.sphero_rvr_async import SpheroRvrAsync
+from Sphero_Locomotion.asyncio.client.dal.serial_async_dal import SerialAsyncDal
 
-from sphero_sdk.observer.observer_base import Observer
-from sphero_sdk.observer.client.firmware.rvr_fw_check_observer import RvrFwCheckObserver
-from sphero_sdk.observer.events.event_dispatcher import EventDispatcher
-from sphero_sdk.observer.controls.led_control_observer import LedControlObserver
-from sphero_sdk.observer.controls.drive_control_observer import DriveControlObserver
-from sphero_sdk.observer.controls.infrared_control_observer import InfraredControlObserver
-from sphero_sdk.observer.controls.sensor_control_observer import SensorControlObserver
-from sphero_sdk.observer.client.dal.observer_parser import ObserverParser
-from sphero_sdk.observer.client.dal.serial_observer_port import SerialObserverPort
-from sphero_sdk.observer.client.dal.serial_observer_dal import SerialObserverDal
-from sphero_sdk.observer.client.toys.sphero_rvr_observer import SpheroRvrObserver
+from Sphero_Locomotion.observer.observer_base import Observer
+from Sphero_Locomotion.observer.client.firmware.rvr_fw_check_observer import RvrFwCheckObserver
+from Sphero_Locomotion.observer.events.event_dispatcher import EventDispatcher
+from Sphero_Locomotion.observer.controls.led_control_observer import LedControlObserver
+from Sphero_Locomotion.observer.controls.drive_control_observer import DriveControlObserver
+from Sphero_Locomotion.observer.controls.infrared_control_observer import InfraredControlObserver
+from Sphero_Locomotion.observer.controls.sensor_control_observer import SensorControlObserver
+from Sphero_Locomotion.observer.client.dal.observer_parser import ObserverParser
+from Sphero_Locomotion.observer.client.dal.serial_observer_port import SerialObserverPort
+from Sphero_Locomotion.observer.client.dal.serial_observer_dal import SerialObserverDal
+from Sphero_Locomotion.observer.client.toys.sphero_rvr_observer import SpheroRvrObserver
 
-from sphero_sdk.common.commands.api_and_shell import *
-from sphero_sdk.common.commands.connection import *
-from sphero_sdk.common.commands.drive import *
-from sphero_sdk.common.commands.io import *
-from sphero_sdk.common.commands.power import *
-from sphero_sdk.common.commands.sensor import *
-from sphero_sdk.common.commands.system_info import *
+from Sphero_Locomotion.common.commands.api_and_shell import *
+from Sphero_Locomotion.common.commands.connection import *
+from Sphero_Locomotion.common.commands.drive import *
+from Sphero_Locomotion.common.commands.io import *
+from Sphero_Locomotion.common.commands.power import *
+from Sphero_Locomotion.common.commands.sensor import *
+from Sphero_Locomotion.common.commands.system_info import *
