0462fa432d2f6067a9bc98d943e2d7d6c13477db
mapfeiff <mapfeiff@uci.edu>
2023-09-12 20:41:33 -0700

    try new method for how to stabilize rvr upon losing sight of object

--- RobotClass/Robot.py
+++ RobotClass/Robot.py
@@ -29,31 +29,31 @@ class Robot():
             if(np.sum(mask/255) < 10):
                 #recenter eyes
                 cur_pan_angle = (self.vision.pan_tilt_unit.controller.PWM_duty_cycles[0]-7.5)/0.055556
-                proportion = 0.3
+                proportion = 0.01
                 robot_proportion_angle_deg = proportion*cur_pan_angle
-                pan_compensation_PWM = 7.5 + 0.055556*(-robot_proportion_angle_deg)
+                pan_compensation_PWM = 7.5 + 0.055556*(robot_proportion_angle_deg)
                 new_pan_PWM = self.vision.pan_tilt_unit.controller.PWM_duty_cycles[0] + pan_compensation_PWM
                 self.vision.pan_tilt_unit.set_servo_PWM_duty_cycles(new_pan_PWM, self.vision.pan_tilt_unit.controller.PWM_duty_cycles[1])
-                #self.rvr.reset_yaw()
-                #self.rvr.reset_locator_x_and_y()
-                #self.rvr.drive_to_position_si(
-                #    linear_speed = 0.5,
-                #    yaw_angle = robot_proportion_angle_deg,
-                #    x = 0,
-                #    y = 0,
-                #    flags = 0
-                #)
+                self.rvr.reset_yaw()
+                self.rvr.reset_locator_x_and_y()
+                self.rvr.drive_to_position_si(
+                    linear_speed = 0.5,
+                    yaw_angle = robot_proportion_angle_deg,
+                    x = 0,
+                    y = 0,
+                    flags = 0
+                )
                 #skip vision processing
-                if(np.abs(robot_proportion_angle_deg) < 7.5):
-                    self.rvr.drive_tank_si_units(
-                        left_velocity = 0,
-                        right_velocity = 0
-                    )
-                else:
-                    self.rvr.drive_tank_si_units(
-                        left_velocity = -robot_proportion_angle_deg/90.0,
-                        right_velocity = robot_proportion_angle_deg/90.0
-                    )
+                #if(np.abs(robot_proportion_angle_deg) < 7.5):
+                #    self.rvr.drive_tank_si_units(
+                #        left_velocity = 0,
+                #        right_velocity = 0
+                #    )
+                #else:
+                #    self.rvr.drive_tank_si_units(
+                #        left_velocity = -robot_proportion_angle_deg/90.0,
+                #        right_velocity = robot_proportion_angle_deg/90.0
+                #    )
                 continue
             mask_center = np.flip((np.array(mask.shape)-1)/2)
             avg_point = self.vision.get_avg_mask_point(mask, relative_point=mask_center)
