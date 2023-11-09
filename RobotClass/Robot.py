8a476e1b4820af6c11d2dc2f0f2bf6f883df4ffd
Cuttlebot <cuttlebot@uci.edu>
2023-11-09 04:40:53 +0000

    added code to get the width of a colored object via contours. The contours dictate if the robot will move forward/backward when facing colored object

--- RobotClass/Robot.py
+++ RobotClass/Robot.py
@@ -23,7 +23,7 @@ class Robot():
 
     def move_to_color(self):
         #Look for red object
-        self.vision.camera.set_color_filter(0, precision=10)
+        self.vision.camera.set_color_filter(0, precision=15)
         #initiallize tank drive speed variabels
         driving_left_velocity = 0
         driving_right_velocity = 0
@@ -34,8 +34,8 @@ class Robot():
             #If there are less than 20 active pixels
             if(np.sum(mask/255) < 20):
                 #stop the robot
-                driving_left_velocity *= 0.9
-                driving_right_velocity *= 0.9
+                driving_left_velocity *= 0.75
+                driving_right_velocity *= 0.75
                 self.rvr.drive_tank_si_units(
                     left_velocity = driving_left_velocity,
                     right_velocity = driving_right_velocity
@@ -45,15 +45,20 @@ class Robot():
             #The mask exists and we know we have found an objects (>=20 active pixels in mask)
             #Get center index in (Row,Col) format
             mask_center = np.flip((np.array(mask.shape)-1)/2)
-            avg_point = self.vision.get_avg_mask_point(mask, relative_point=mask_center)
+            #avg_point = self.vision.get_avg_mask_point(mask, relative_point=mask_center)
+            #Get the largest contour for the mask and find the centroid relative to the center of the mask
+            largest_contour = self.vision.get_largest_contour(mask)
+            largest_contour_bounding_box = self.vision.get_contour_bounding_box(largest_contour)
+            largest_contour_centroid = self.vision.get_contour_bounding_box_centroid(largest_contour_bounding_box)
+            rel_point = self.vision.get_relative_position(largest_contour_centroid, relative_point=mask_center)
             #Now update the pan tilt unit according to the output of the control system (avg_point); with reference point at (0,0)
-            self.vision.pan_tilt_unit.update(avg_point)
+            self.vision.pan_tilt_unit.update(rel_point)
             #Now move the robot according to the current angle of the pan unit
-            cur_pan_angle = (self.vision.pan_tilt_unit.controller.PWM_duty_cycles[0]-7.5)/0.055556
-            proportion = 0.4
+            cur_pan_angle = (self.vision.pan_tilt_unit.controller.PWM_duty_cycles[0]-7.5)/0.055556 #!!!need to add servo class to PTU so we can get the servo angle
+            proportion = 0.3
             robot_proportion_angle_deg = proportion*cur_pan_angle
             #Filter out small robot movements
-            if(np.abs(robot_proportion_angle_deg) < 2.5):
+            if(np.abs(robot_proportion_angle_deg) < 1.5):
                 driving_left_velocity = 0
                 driving_right_velocity = 0
             #if the movement is big enough, then move the robot
@@ -61,9 +66,11 @@ class Robot():
                 driving_left_velocity = -robot_proportion_angle_deg/90.0
                 driving_right_velocity = robot_proportion_angle_deg/90.0
             #in addition to turning the robot, add an offset to move it forward toward the object of intetest
-            driving_left_velocity += 0.1
-            driving_right_velocity += 0.1
-            
+            velocity_limit = 0.1 #m/s
+            optimal_object_width = 150
+            driving_left_velocity += velocity_limit*(1.0 - largest_contour_bounding_box[2]/optimal_object_width)
+            driving_right_velocity += velocity_limit*(1.0 - largest_contour_bounding_box[2]/optimal_object_width)
+            #after computing the new velocity of the wheels, set the values to the rvr
             self.rvr.drive_tank_si_units(
                     left_velocity = driving_left_velocity,
                     right_velocity = driving_right_velocity
--- RobotClass/Vision/Perception.py
+++ RobotClass/Vision/Perception.py
@@ -40,7 +40,7 @@ class Perception():
         cv2.destroyAllWindows()
 
     #Helper function to get the average pixel of a given mask
-    def get_avg_mask_point(self, mask, relative_point=(0,0)):
+    def get_avg_mask_point(self, mask, relative_point=np.array([0,0])):
         #Get the dimensions of the mask
         (num_rows, num_cols) = mask.shape
         #Get the weights for the rows and columns
@@ -57,3 +57,35 @@ class Perception():
         #Then compute the average point relative to some pixel
         avg_point -= relative_point
         return(avg_point)
+
+    #return the absolute point with measurementsthat are relative to the relative point
+    def get_relative_position(self, absolute_point, relative_point=np.array([0,0])):
+        #point will be the absolute point minus the relative point (relative point is the reference)
+        return(absolute_point - relative_point)
+
+    #return the largest contour from a given mask
+    #modified from: https://stackoverflow.com/questions/44588279/find-and-draw-the-largest-contour-in-opencv-on-a-specific-color-python
+    def get_largest_contour(self, mask):
+        #first blur the image so that there are no errors with getting the contour
+        blurred_mask = cv2.blur(mask, (15,15))
+        #then get the threshold above 50% brightness
+        ret, thresh = cv2.threshold(blurred_mask, 127, 255, 0)
+        #next, find all the contours in the image
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #last, if a contour exist, find the largest one
+        if(len(contours) > 0):
+            return(max(contours, key = cv2.contourArea))
+        #if no contours found, return none
+        return(None)
+
+    #return the bounding box for the specificed contour
+    def get_contour_bounding_box(self, contour):
+        #return bounding box in x,y,w,h form
+        return(cv2.boundingRect(contour))
+
+    #get the centroid for the bounding box
+    def get_contour_bounding_box_centroid(self, contour_bounding_box):
+        #compute the centroids in the x and y direction
+        x_center = np.round(contour_bounding_box[0] + contour_bounding_box[2]/2)
+        y_center = np.round(contour_bounding_box[1] + contour_bounding_box[3]/2)
+        return(np.array([x_center, y_center]))
